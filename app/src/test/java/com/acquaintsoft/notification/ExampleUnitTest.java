ae0ce5597bdbf3b6adaa6ec561a355f70d5f3ec9
1986webdeveloper <1986webdeveloper@gmail.com>
2019-08-01 17:16:07 +0530

    final code

--- app/src/test/java/com/acquaintsoft/notification/ExampleUnitTest.java
+++ app/src/test/java/com/acquaintsoft/notification/ExampleUnitTest.java
@@ -1,17 +1,17 @@
-package com.acquaintsoft.notification
+package com.acquaintsoft.notification;
 
-import org.junit.Test
+import org.junit.Test;
 
-import org.junit.Assert.*
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
+ * <p>
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
